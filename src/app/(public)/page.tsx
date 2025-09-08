0bde28f12f454ec8b1121c053de89a637affeb19
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-08 21:33:55 +0330

    refactor: move HomePage to public route group  and integrate TopNav + Overlay
    Relocated main page  and composed layout with TopNav and Overlay components. Applied full-screen background image and flex column structure.

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,11 @@
+import Overlay from "@/components/landing-page/Overlay";
+import TopNav from "@/components/landing-page/TopNav";
+
+export default function HomePage() {
+  return (
+    <div className="w-screen h-screen background bg-[url(/assets/header-bg.jpg)] bg-bottom-right bg-no-repeat bg-cover flex flex-col">
+      <TopNav />
+      <Overlay />
+    </div>
+  );
+}
