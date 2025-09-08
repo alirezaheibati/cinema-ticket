cd87b7fc68f4e15a19880b865911314908ee70f1
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-08 21:28:07 +0330

    feat: add Overlay component with Persian CTA and intro text
    Created a centered overlay with semi-transparent background, Persian headline, and descriptive paragraph. Includes a رزرو بلیط button using ShadCN's secondary variant.

---
+++ src/components/landing-page/Overlay.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+
+export default function Overlay() {
+  return (
+    <div className="w-full grow flex justify-center items-center">
+      <div className="bg-black/30 rounded-lg px-8 py-10 text-white/90 w-lg max-w-screen">
+        <h1 className="text-center font-semibold text-xl">
+          بزرگ ترین مرجع <span className="text-destructive">رزرو بلیط</span>{" "}
+          سینما در ایران
+        </h1>
+        <p className="text-center text-sm mt-4 mb-6 text-gray-400">
+          همکاری با برترین سینماهای کشور، ارایه‌ی خدمات ویژه به سازمان‌ها و
+          شرکت‌های دولتی و خصوصی به منظور اکران‌های خصوصی، فروش بلیت جشنواره
+          فیلم فجر و تسهیل اتصال و ارتباط با درگاه یک‌پارچه‌ی سمفا برای سایر
+          کسب‌وکارها
+        </p>
+        <Button variant={"secondary"} className="block mx-auto cursor-pointer">
+          رزرو بلیط
+        </Button>
+      </div>
+    </div>
+  );
+}
