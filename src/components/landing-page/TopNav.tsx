25bda6ab032d63822899fe7c11c97ea5d6786c21
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-14 19:45:09 +0330

    feat: add TopNav component with sheet-based auth forms
    Displays logo and signup button, opens sheet with login or register form based on URL query parameter.

--- src/components/landing-page/TopNav.tsx
+++ src/components/landing-page/TopNav.tsx
@@ -1,10 +1,45 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useState } from "react";
+import RegisterForm from "../auth/Register";
+import LoginFrom from "../auth/Login";
+import Logo from "../ui/Logo";
+
 export default function TopNav() {
+  let form = null;
+  const [sheetVisibility, setSheetVisibility] = useState(false);
+  if (typeof window !== "undefined") {
+    const queryString = new URLSearchParams(window.location.search);
+    form = queryString.get("form");
+  }
+  function openSheetHandler() {
+    setSheetVisibility(true);
+  }
   return (
     <header className="px-10 py-5 flex justify-between items-center w-full">
-      <Button variant={"secondary"}>ثبت نام</Button>
-      <Image src={"/assets/logo.png"} alt="site logo" width={60} height={60} />
+      <Button variant={"secondary"} onClick={openSheetHandler}>
+        ثبت نام
+      </Button>
+      <Logo />
+      <Sheet
+        open={sheetVisibility}
+        onOpenChange={(open) => setSheetVisibility(open)}
+      >
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle></SheetTitle>
+            <div className="flex justify-center items-center h-screen flex-col">
+              {form === "register" ? <RegisterForm /> : <LoginFrom />}
+            </div>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 }
