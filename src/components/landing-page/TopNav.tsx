c0c3330cbc8f283738729085af7a5b19675bf2f1
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-17 14:15:10 +0330

    refactor: use useSearchParams hook to get the search param and render relevant form with it

--- src/components/landing-page/TopNav.tsx
+++ src/components/landing-page/TopNav.tsx
@@ -6,36 +6,44 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RegisterForm from "../auth/Register";
 import LoginFrom from "../auth/Login";
 import Logo from "../ui/Logo";
+import { useSearchParams } from "next/navigation";
 
 export default function TopNav() {
-  let form = null;
+  const [activeFrom, setActiveForm] = useState<"login" | "register">();
   const [sheetVisibility, setSheetVisibility] = useState(false);
-  if (typeof window !== "undefined") {
-    const queryString = new URLSearchParams(window.location.search);
-    form = queryString.get("form");
-  }
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    const searchParam =
+      searchParams.get("form") === "login"
+        ? "login"
+        : searchParams.get("form") === "register"
+        ? "register"
+        : "login";
+
+    setActiveForm(searchParam);
+  }, [searchParams]);
   function openSheetHandler() {
     setSheetVisibility(true);
   }
   return (
     <header className="px-10 py-5 flex justify-between items-center w-full">
       <Button variant={"secondary"} onClick={openSheetHandler}>
-        ثبت نام
+        ثبت نام / ورود
       </Button>
       <Logo />
       <Sheet
         open={sheetVisibility}
         onOpenChange={(open) => setSheetVisibility(open)}
       >
-        <SheetContent>
+        <SheetContent className="">
           <SheetHeader>
             <SheetTitle></SheetTitle>
             <div className="flex justify-center items-center h-screen flex-col">
-              {form === "register" ? <RegisterForm /> : <LoginFrom />}
+              {activeFrom === "register" ? <RegisterForm /> : <LoginFrom />}
             </div>
           </SheetHeader>
         </SheetContent>
