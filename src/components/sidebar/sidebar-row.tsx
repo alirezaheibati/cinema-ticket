c1a6f1efc813226fcb9c1724976a71ebd6aebdf2
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-17 17:26:45 +0330

    feat: close sidebar on nav item click via onClickNavItem prop  Triggered sidebar close by setting state to false on nav item click

--- src/components/sidebar/sidebar-row.tsx
+++ src/components/sidebar/sidebar-row.tsx
@@ -1,17 +1,27 @@
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { JSX } from "react";
+import { Dispatch, JSX, SetStateAction } from "react";
 interface sidebarRowProps {
   title: string;
   path: string;
   icon: JSX.Element;
+  onclickNavItem: Dispatch<SetStateAction<boolean>>;
 }
-export default function SidebarRow({ title, path, icon }: sidebarRowProps) {
+export default function SidebarRow({
+  title,
+  path,
+  icon,
+  onclickNavItem,
+}: sidebarRowProps) {
   const pathname = usePathname();
   return (
     <li
+      onClick={() => onclickNavItem(false)}
       className={` ${
-        path === pathname ? "border border-primary bg-primary/10" : "border-0"
+        pathname.startsWith(path)
+          ? "border border-primary bg-primary/10"
+          : "border-0"
       } w-full  overflow-hidden rounded-sm p-3 `}
     >
       <Link href={path} className="flex items-center gap-3 justify-start">
