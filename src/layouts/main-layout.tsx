e36243b488f07be7fd7d7a27a749fda638f93a18
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-20 12:02:14 +0330

    feat: add user info retrieval logic to MainLayout with session fallback

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,22 +1,38 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { ReactNode, useEffect } from "react";
 import AdminLayout from "./admin-layout";
 import UserLayout from "./user-layout";
 import PrivateHeader from "@/layouts/private-header";
 import { retriveLogedInUser } from "@/actions/users";
 import { useUserStore } from "@/store/user-store";
-
+import cookies from "js-cookie";
+import { toast } from "sonner";
 export default function MainLayout({ children }: { children: ReactNode }) {
-  const { setUser } = useUserStore();
+  const { setUser, user } = useUserStore();
+  const router = useRouter();
   async function retriveUser() {
-    const data = await retriveLogedInUser();
-    setUser(data.user);
+    try {
+      if (!user) {
+        const data = await retriveLogedInUser();
+        if (!data.success) {
+          throw new Error("اطلاعات حساب یافت نشد. دوباره لاگین کنید.");
+        }
+        setUser(data.user);
+      }
+    } catch (err: any) {
+      toast.error(err.message);
+      cookies.remove("jwt-token");
+      cookies.remove("user-role");
+      setTimeout(() => {
+        router.push("/?form=login");
+      }, 3000);
+    }
   }
   useEffect(() => {
     retriveUser();
-  }, []);
+  }, [user]);
   const pathname = usePathname();
   if (pathname.startsWith("/admin")) {
     return (
