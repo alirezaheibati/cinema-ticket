0ef7406304cb1e1813eebd37031adbe839d92b53
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-23 17:41:31 +0330

    feat: implement AdminSidebar with dynamic menu and Sheet layout
    Includes Persian-labeled navigation items, lucide-react icons, and logout functionality inside Sheet UI.

---
+++ src/components/sidebar/admin-sidebar.tsx
@@ -0,0 +1,59 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import {
+  Clapperboard,
+  Drama,
+  LayoutDashboard,
+  MonitorPlay,
+  TicketCheck,
+  UserRoundPen,
+} from "lucide-react";
+import LogoutBtn from "./logout-btn";
+import SidebarRow from "./sidebar-row";
+export default function AdminSidebar({
+  openSidebar,
+  setOpenSidebar,
+}: {
+  openSidebar: boolean;
+  setOpenSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const adminMenuItems = [
+    { title: "داشبورد", icon: <LayoutDashboard />, path: "/admin/dashboard" },
+    { title: "فیلم", icon: <Clapperboard />, path: "/admin/movies" },
+    { title: "تئاتر", icon: <Drama />, path: "/admin/theater" },
+    { title: "سریال", icon: <MonitorPlay />, path: "/admin/shows" },
+    { title: "رزروها", icon: <TicketCheck />, path: "/admin/reserves" },
+    { title: "کاربران", icon: <UserRoundPen />, path: "/admin/users" },
+  ];
+  return (
+    <>
+      <Sheet open={openSidebar} onOpenChange={(open) => setOpenSidebar(open)}>
+        <SheetContent>
+          <SheetTitle />
+          <SheetHeader />
+          <SheetDescription />
+          <ul className="flex px-4 flex-col justify-start items-start gap-2 w-full">
+            {adminMenuItems.map((item) => (
+              <SidebarRow
+                key={item.path}
+                title={item.title}
+                path={item.path}
+                icon={item.icon}
+                onclickNavItem={setOpenSidebar}
+              />
+            ))}
+          </ul>
+          <SheetFooter>
+            <LogoutBtn />
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+}
