5783cd985ec91a095c4e948d97c81b7f8f87daa4
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-23 17:54:52 +0330

    refactor: implement AdminLayout with toggleable sidebar and header branding
    Includes lucide-react Menu icon for sidebar control and Logo component for consistent admin branding.

--- src/layouts/admin-layout.tsx
+++ src/layouts/admin-layout.tsx
@@ -1,5 +1,25 @@
+"use client";
 import { ReactNode } from "react";
-
+import Logo from "@/components/ui/Logo";
+import { Menu } from "lucide-react";
+import { useState } from "react";
+import AdminSidebar from "@/components/sidebar/admin-sidebar";
 export default function AdminLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  const [openSidebar, setOpenSidebar] = useState(false);
+  return (
+    <>
+      <div className="flex justify-between items-center px-10 py-3 bg-primary">
+        <div className="flex justify-start items-center gap-5">
+          <Menu
+            size={24}
+            className="text-white/90 cursor-pointer"
+            onClick={() => setOpenSidebar(true)}
+          />
+        </div>
+        <Logo />
+      </div>
+      {openSidebar && <AdminSidebar {...{ openSidebar, setOpenSidebar }} />}
+      {children}
+    </>
+  );
 }
