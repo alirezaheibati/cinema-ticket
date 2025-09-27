f7e1bf9c5b2f9c66a93bcce4cc43058e004467db
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-27 16:04:37 +0330

    feat: add SuccessPage layout with centered OrderDetails component

---
+++ src/app/(private)/user/success/page.tsx
@@ -0,0 +1,9 @@
+import OrderDetails from "@/components/success/order-details";
+
+export default function SeccessPage() {
+  return (
+    <section className="max-w-7xl px-4 mx-auto py-8 flex justify-center items-center h-[calc(100vh-85px)]">
+      <OrderDetails />
+    </section>
+  );
+}
