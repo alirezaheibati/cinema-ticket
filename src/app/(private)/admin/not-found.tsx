ff0cb2b7114b9f72d972ee1761e8579cd50ad6e6
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-28 20:28:50 +0330

    feat: add custom admin notFound page with themed 404 layout
    Displays Nemo image and localized message with redirect link to admin dashboard.

---
+++ src/app/(private)/admin/not-found.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import nemoImage from "@/../public/assets/nemo.png";
+import Link from "next/link";
+export default function notFoundPage() {
+  return (
+    <div className="w-screen h-[calc(100vh-85px)] bg-gray-50 relative">
+      <div className="w-full h-full absolute flex flex-col justify-center items-center p-4 sm:p-8">
+        <p className="font-bold text-9xl opacity-10 leading-0 bg-red-100 mb-8">
+          404
+        </p>
+        <Image src={nemoImage} alt="nemo the fish image" width={200} />
+        <p className="font-semibold mb-1">صفحه مورد نظر یافت نشد.</p>
+        <Link
+          href={"/admin/dashboard"}
+          className="bg-destructive px-4 py-2 text-popover rounded-md"
+        >
+          داشبورد
+        </Link>
+      </div>
+    </div>
+  );
+}
