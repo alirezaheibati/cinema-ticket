7cbba93c3628460901f2fcb700989342c8253331
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-28 21:31:10 +0330

    fix: wrap TopNav in Suspense to support useSearchParams
    Resolves hydration error by suspending client hook usage; ensures proper rendering of login/register sheet.

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,12 @@
 import Overlay from "@/components/landing-page/Overlay";
 import TopNav from "@/components/landing-page/TopNav";
-
+import { Suspense } from "react";
 export default function HomePage() {
   return (
     <div className="w-screen h-screen background bg-[url(/assets/header-bg.jpg)] bg-bottom-right bg-no-repeat bg-cover flex flex-col">
-      <TopNav />
+      <Suspense fallback={null}>
+        <TopNav />
+      </Suspense>
       <Overlay />
     </div>
   );
