eaf0a8d5cd1ed0210fb9be879075387bd5658024
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2025-09-29 12:02:09 +0330

    style: fix the loading spinner width and height

--- src/app/(private)/loading.tsx
+++ src/app/(private)/loading.tsx
@@ -1,7 +1,7 @@
 export default function loadingPage() {
   return (
     <div className="h-screen w-screen flex justify-center items-center bg-white">
-      <div className="border-8 rounded-full border-t-transparent border-primary animate-spin"></div>
+      <div className="h-16 w-16 border-8 rounded-full border-t-transparent border-primary animate-spin"></div>
     </div>
   );
 }
